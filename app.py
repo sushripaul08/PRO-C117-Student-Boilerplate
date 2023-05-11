4cc88db81c3497447e92e738409a5138cb5f26e9
Sushri Paul <90126216+sushripaul08@users.noreply.github.com>
2023-05-11 17:48:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
     return render_template('index.html')
 
 # write a route for post request
-@app.route('' , methods = [''])
+@app.route('/' , methods = ['POST'])
 def review():
 
     # extract the customer_review by writing the appropriate 'key' from the JSON data
